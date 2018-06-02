3424e4a4fbcb41f170b05912c2ab8ed8dc7d4fa8
Duy <duy@Duys-MacBook-Air.local>
2018-06-02 11:19:30 +0700

    Hiho

--- session4/homework/ex1.py
+++ session4/homework/ex1.py
@@ -1,7 +1,6 @@
 name = input("your full name: ")
 names = name.strip().split()
 split_name = []
-standard_name = []
 for item in names:
     split_name.append(item.lower()) 
 standard_name = " ".join(split_name)
